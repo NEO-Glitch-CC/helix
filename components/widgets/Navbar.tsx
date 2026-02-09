80dad75dc0a84f97b062a1ec2fea7223dd98f17a
lnvvalerie60 <lnvvalerie60@gmail.com>
2026-02-09 01:51:54 +0000

    v0.0.0.2

---
+++ components/widgets/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 z-50 inset-x-0 w-full h-[56px] bg-neutral-950">
+      <div className="w-full flex items-center justify-between">
+        
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
